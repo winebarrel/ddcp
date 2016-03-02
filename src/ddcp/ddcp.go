274b393aaf265437e444b4ccc8642178a89d2c4c
winebarrel <sgwr_dts@yahoo.co.jp>
2016-03-03 02:05:42 +0900

    run dd without sh

--- src/ddcp/ddcp.go
+++ src/ddcp/ddcp.go
@@ -1,41 +1,40 @@
 package ddcp
 
 import (
-	"bytes"
 	"fmt"
 	"os"
 	"os/exec"
 	"strconv"
 )
 
-func ddCmdOpes(opes map[string]string) string {
-	buf := bytes.NewBufferString("dd")
+func ddOpes(opes map[string]string) (cmd_opes []string) {
+	cmd_opes = []string{}
 
 	for k, v := range opes {
-		buf.WriteString(" ")
-		buf.WriteString(k)
-		buf.WriteString("=")
-		buf.WriteString(v)
+		cmd_opes = append(cmd_opes, fmt.Sprintf("%s=%s", k, v))
 	}
 
-	return buf.String()
+	return
 }
 
-func ddOpes(source string, dest string, bs string, count string) map[string]string {
-	return map[string]string{
-		"if":    source,
-		"of":    dest,
-		"conv":  "notrunc",
-		"bs":    bs,
-		"count": count}
-}
+func ddOpesList(source string, dest string, chunk_size int64, chunk_num int64, remainder int64) (opes_list [][]string) {
+	if remainder > 0 {
+		chunk_num++
+	}
 
-func ddCmds(source string, dest string, chunk_size int64, chunk_num int64) (cmds []string) {
-	cmds = make([]string, chunk_num)
+	opes_list = make([][]string, chunk_num)
 	chunk_size_mb := chunk_size / (1024 * 1024)
 
 	for i := int64(0); i < chunk_num; i++ {
-		opes := ddOpes(source, dest, "1m", strconv.FormatInt(chunk_size_mb, 10))
+		opes := map[string]string{
+			"if":   source,
+			"of":   dest,
+			"conv": "notrunc",
+			"bs":   "1m"}
+
+		if i < chunk_num-1 {
+			opes["count"] = strconv.FormatInt(chunk_size_mb, 10)
+		}
 
 		if i > 0 {
 			offset := strconv.FormatInt(chunk_size_mb*i, 10)
@@ -43,30 +42,30 @@ func ddCmds(source string, dest string, chunk_size int64, chunk_num int64) (cmds
 			opes["seek"] = offset
 		}
 
-		cmds[i] = ddCmdOpes(opes)
+		opes_list[i] = ddOpes(opes)
 	}
 
 	return
 }
 
-func runCmd(cmd string, ch chan error) {
-	out, err := exec.Command("sh", "-c", cmd).CombinedOutput()
+func dd(opes []string, ch chan error) {
+	out, err := exec.Command("dd", opes...).CombinedOutput()
 
 	if err != nil {
-		err = fmt.Errorf("'%s' is failed: %s", cmd, out)
+		err = fmt.Errorf("'dd %s' is failed: %s", opes, out)
 	}
 
 	ch <- err
 }
 
-func runCmds(cmds []string) (err error) {
+func runCmds(opes_list [][]string) (err error) {
 	ch := make(chan error)
 
-	for _, cmd := range cmds {
-		go runCmd(cmd, ch)
+	for _, opes := range opes_list {
+		go dd(opes, ch)
 	}
 
-	for _ = range cmds {
+	for _ = range opes_list {
 		err = <-ch
 
 		if err != nil {
@@ -99,15 +98,7 @@ func Ddcp(params *DdcpParams) error {
 	remainder := src_size % params.chunk_size
 	chunk_num := src_size / params.chunk_size
 
-	cmds := ddCmds(params.source, params.dest, params.chunk_size, chunk_num)
+	opes_list := ddOpesList(params.source, params.dest, params.chunk_size, chunk_num, remainder)
 
-	if remainder > 0 {
-		opes := ddOpes(params.source, params.dest, strconv.FormatInt(remainder, 10), "1")
-		offset := strconv.FormatInt(params.chunk_size*chunk_num, 10)
-		opes["skip"] = offset
-		opes["seek"] = offset
-		cmds = append(cmds, ddCmdOpes(opes))
-	}
-
-	return runCmds(cmds)
+	return runCmds(opes_list)
 }
